2fdbc6d7bc96b192ea561a8fb7d24287b2f331f2
gabramson <gabramson@showingtime.com>
2018-02-20 08:44:08 -0600

    Reducer working

--- src/store/tournament/__tests__/reducer.spec.js
+++ src/store/tournament/__tests__/reducer.spec.js
@@ -20,10 +20,23 @@ describe('store/tournament/reducer', () => {
     });
 
     it('should advance game 1, top line', () => {
-        const startState = Immutable([1,2,0]);
-        const endState = Immutable([1,2,1]);
+        const startState = Immutable([1, 2, 3, 4, 0, 0, 0]);
+        const endState = Immutable([1, 2, 3, 4, 1, 0, 0]);
         const action = {type: types.ADVANCE_TEAM, game: 1, line: types.lines.TOP};
         Reducer(uut).withState(startState).expect(action).toReturnState(endState);
+    });
+
+    it('should play mini-tornament correctly', () => {
+        const startState = Immutable([1, 2, 3, 4, 0, 0, 0]);
+        const game1Action = {type: types.ADVANCE_TEAM, game: 1, line: types.lines.TOP};
+        const game1NextState = Immutable([1, 2, 3, 4, 1, 0, 0]);
+        const game2Action = {type: types.ADVANCE_TEAM, game: 2, line: types.lines.BOTTOM};
+        const game2NextState = Immutable([1, 2, 3, 4, 1, 4, 0]);
+        const game3Action = {type: types.ADVANCE_TEAM, game: 3, line: types.lines.BOTTOM};
+        const game3NextState = Immutable([1, 2, 3, 4, 1, 4, 4]);
+        Reducer(uut).withState(startState).expect(game1Action).toReturnState(game1NextState);
+        Reducer(uut).withState(game1NextState).expect(game2Action).toReturnState(game2NextState);
+        Reducer(uut).withState(game2NextState).expect(game3Action).toReturnState(game3NextState);
     });
 });
     
--- src/store/tournament/reducer.js
+++ src/store/tournament/reducer.js
@@ -4,12 +4,16 @@ import Immutable from 'seamless-immutable';
 const initialState = Immutable([1,2,0]);
 
 export default function reduce(state = initialState, action = {}) {
+    const n = state.length;
+    const rounds = Math.log2(n+1)-1;
+    const currentSlot = 2*(action.game - 1) + action.line;
+    const nextSlot = Math.floor(currentSlot/2) + Math.pow(2, rounds)
     switch (action.type){
         case types.ADVANCE_TEAM:
             return([
-                ...state.slice(0, 2),
-                state[0],
-                ...state.slice(3, 0)
+                ...state.slice(0, nextSlot),
+                state[currentSlot],
+                ...state.slice(nextSlot + 1, n)
             ]);
         default:
             return state;
