85629211d4c2e66f97187fe331d89f8fc156ecb4
gabramson <gabramson@showingtime.com>
2019-03-20 22:07:08 -0500

    can't clear first round

--- src/components/line.js
+++ src/components/line.js
@@ -3,9 +3,11 @@ import React from 'react';
 export default function line({onClick, onClearClick, text}) {
     return (
       <div className="Line">
+      {typeof(text) !== "undefined" && text.substring(0,1) !== "_" &&
         <span className="Clear" onClick={onClearClick}>
           x
         </span>
+      }
         <span>&nbsp;</span>
         <span onClick={onClick}>
           {text}
